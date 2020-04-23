d351c88a813bb059d88374ef4d624473179e205d
n-motawea <magedmotawea@gmail.com>
2020-04-23 23:08:28 +0200

    complete ARP learning and add a mgmt port in env

--- l2/arp.go
+++ l2/arp.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net"
+	"time"
 
 	"github.com/BurntSushi/toml"
 	"github.com/m-motawea/gSwitch/controlplane"
@@ -13,6 +14,8 @@ import (
 	"github.com/mdlayher/ethernet"
 )
 
+const ARP_EXPIRE_TIME = 60 * time.Second
+
 type LocalAddress struct {
 	IP  string
 	MAC string
@@ -20,6 +23,102 @@ type LocalAddress struct {
 
 type ARPConfig struct {
 	LocalAddresses map[string]LocalAddress
+}
+
+type ARPEntry struct {
+	IP            net.IP
+	MAC           net.HardwareAddr
+	Port          *dataplane.SwitchPort
+	TimeCreated   time.Time
+	LastRefreshed time.Time
+}
+
+func (ae *ARPEntry) Refresh() {
+	ae.LastRefreshed = time.Now()
+}
+
+func (ae *ARPEntry) IsExpired() bool {
+	return ae.LastRefreshed.Add(ARP_EXPIRE_TIME).Sub(time.Now()) < time.Second
+}
+
+type SwitchARPTable struct {
+	ARPTable        map[string]*ARPEntry   // IP to one ARP Entry
+	InverseARPTable map[string][]*ARPEntry // MAC to multiple ARP Entries
+}
+
+func (at *SwitchARPTable) SetEntry(ip net.IP, mac net.HardwareAddr, port *dataplane.SwitchPort) *ARPEntry {
+	log.Printf("ARP Process: Setting Entry for IP: %v, MAC: %v, PORT: %v", ip, mac, port)
+	t := time.Now()
+	ent := ARPEntry{
+		IP:            ip,
+		MAC:           mac,
+		Port:          port,
+		TimeCreated:   t,
+		LastRefreshed: t,
+	}
+	strMAc := mac.String()
+	at.ARPTable[ip.String()] = &ent
+	val, ok := at.InverseARPTable[strMAc]
+	if !ok {
+		val = []*ARPEntry{}
+	}
+	val = append(val, &ent)
+	at.InverseARPTable[strMAc] = val
+	return &ent
+}
+
+func (at *SwitchARPTable) GetEntry(ip net.IP) *ARPEntry {
+	ipStr := ip.String()
+	ent, ok := at.ARPTable[ipStr]
+	if !ok {
+		return nil
+	}
+	return ent
+}
+
+func (at *SwitchARPTable) DelEntry(ip net.IP) {
+	ipStr := ip.String()
+	ent, ok := at.ARPTable[ipStr]
+	if !ok {
+		return
+	}
+	mac := ent.MAC
+	invMacList := at.InverseARPTable[mac.String()]
+
+	var index int
+	for i, invEnt := range invMacList {
+		if ent == invEnt {
+			index = i
+			break
+		}
+	}
+	invMacList = append(invMacList[:index], invMacList[index+1:]...)
+	delete(at.ARPTable, ipStr)
+}
+
+func (at *SwitchARPTable) ClearExpired() {
+	for _, ent := range at.ARPTable {
+		log.Printf("ARP Process: Checking Entry %v", ent)
+		if ent.IsExpired() {
+			log.Printf("ARP Process: Entry %v Expired. Clearing..", ent)
+			at.DelEntry(ent.IP)
+		}
+	}
+}
+
+func (at *SwitchARPTable) CheckAndClear() {
+	log.Println("ARP Process: Starting ARP Table Check and Clear Routine..")
+	for {
+		timer := time.NewTimer(MAC_EXPIRE_TIME)
+		<-timer.C
+		at.ClearExpired()
+		log.Printf("ARP Process: current ARP Table %v", at.ARPTable)
+	}
+}
+
+func (at *SwitchARPTable) Init() {
+	at.ARPTable = map[string]*ARPEntry{}
+	at.InverseARPTable = map[string][]*ARPEntry{}
 }
 
 func readConfig(path string) (ARPConfig, error) {
@@ -73,11 +172,14 @@ func InitARP(sw *controlplane.Switch) {
 			}
 		}
 	}
+	arpTable := SwitchARPTable{}
+	arpTable.Init()
+	stor["Table"] = arpTable
+	go arpTable.CheckAndClear()
 }
 
 func ReplyARPIn(proc pipeline.PipelineProcess, msg pipeline.PipelineMessage) pipeline.PipelineMessage {
 	// This Process handles ARP requests destined to the switch and populate the ARP Table
-	// TODO Populate ARP Table of all L2 Frames
 	msgContent, _ := msg.Content.(controlplane.ControlMessage)
 	stor := msgContent.ParentSwitch.Stor.GetStor(2, "ARP")
 	config, ok := stor["CONFIG"].(ARPConfig)
@@ -90,14 +192,40 @@ func ReplyARPIn(proc pipeline.PipelineProcess, msg pipeline.PipelineMessage) pip
 	frame := msgContent.InFrame.FRAME
 	log.Printf("ARP Reply proc Frame of Type %v", frame.EtherType.String())
 	if frame.EtherType == ethernet.EtherTypeARP {
-		log.Println("ARP Reply: this is arp request.")
+		log.Println("ARP Process: this is arp frame.")
 		p := new(arp.Packet)
 		if err := p.UnmarshalBinary(frame.Payload); err != nil {
 			log.Println("ARP Process Received invalid ARP Packet")
 			return msg
 		}
+
+		stor := msgContent.ParentSwitch.Stor.GetStor(2, "ARP")
+		table := stor["Table"].(SwitchARPTable)
+
 		targetIP := p.TargetIP.String()
-		log.Printf("ARP Process: ARP Request for IP: %s", targetIP)
+
+		if p.Operation == arp.OperationReply {
+			log.Println("ARP Process: This is ARP Reply")
+			// ARP Reply
+			// Add target ip and mac to ARP Table
+			table.SetEntry(p.TargetIP, p.TargetHardwareAddr, msgContent.InFrame.IN_PORT)
+			// if it is for a local address drop
+			for _, addr := range config.LocalAddresses {
+				if addr.IP == targetIP {
+					log.Printf("ARP Process: This is ARP Reply to My IP: %v", addr.IP)
+					msg.Drop = true
+				}
+			}
+			return msg
+		} else {
+			// ARP Request
+			// Add Sender MAC and IP to ARP table
+			log.Println("ARP Process: This is ARP Request")
+			table.SetEntry(p.SenderIP, p.SenderHardwareAddr, msgContent.InFrame.IN_PORT)
+		}
+
+		log.Printf("ARP Process: Target IP: %s", targetIP)
+
 		for iface, addr := range config.LocalAddresses {
 			log.Printf("ARP Process: checking addr: %s", addr.IP)
 			mac, err := net.ParseMAC(addr.MAC)
--- scripts/env_setup.sh
+++ scripts/env_setup.sh
@@ -14,12 +14,15 @@ ip link add h2 type veth peer name sw2
 ip link add h3 type veth peer name sw3
 ip link add h4 type veth peer name sw4
 ip link add h5 type veth peer name sw5
+ip link add sw type veth peer name sw-mgmt # veth pair used both ends in sw namepace to reach running processes on this namespace
 
 ip link set netns sw sw1
 ip link set netns sw sw2
 ip link set netns sw sw3
 ip link set netns sw sw4
 ip link set netns sw sw5
+ip link set netns sw sw
+ip link set netns sw sw-mgmt
 
 ip link set netns h1 h1
 ip link set netns h2 h2
@@ -45,6 +48,8 @@ ip -n sw link set dev sw2 up
 ip -n sw link set dev sw3 up
 ip -n sw link set dev sw4 up
 ip -n sw link set dev sw5 up
+ip -n sw link set dev sw-mgmt up
+ip -n sw link set dev sw up
 ip -n sw link set dev sw5.1 up
 ip -n sw link set dev sw5.10 up
 ip -n h5 link set dev h5.1 up
@@ -56,3 +61,4 @@ ip -n h3 addr add 10.10.1.30/24 dev h3
 ip -n h4 addr add 10.10.1.40/24 dev h4
 ip -n h5 addr add 10.1.1.50/24 dev h5.1
 ip -n h5 addr add 10.10.1.50/24 dev h5.10
+ip -n sw addr add 10.1.1.254/24 dev sw
